37f70c7ffa02808af5ae831492757839853b142e
yuizho <yuizho3@gmail.com>
2020-05-31 22:06:19 +0900

    rewrite with Router Hooks

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -7,23 +7,25 @@ import { ramenData } from "../../ramenData";
 
 const codes = Object.keys(ramenData);
 
-const Home: FC<{}> = () => (
-  <>
-    <Helmet>
-      <title>ramen home</title>
-    </Helmet>
-    <header>
-      <h1>ramen</h1>
-      <p>This is a content about ramen.</p>
-    </header>
-    <List as="ul">
-      {codes.map((code) => (
-        <List.Item as="li" key={code}>
-          <Link to={`/ramens/${code}`}>{ramenData[code].region}</Link>
-        </List.Item>
-      ))}
-    </List>
-  </>
-);
+const Home: FC = () => {
+  return (
+    <>
+      <Helmet>
+        <title>ramen home</title>
+      </Helmet>
+      <header>
+        <h1>ramen</h1>
+        <p>This is a content about ramen.</p>
+      </header>
+      <List as="ul">
+        {codes.map((code) => (
+          <List.Item as="li" key={code}>
+            <Link to={`/ramens/${code}`}>{ramenData[code].region}</Link>
+          </List.Item>
+        ))}
+      </List>
+    </>
+  );
+};
 
 export default Home;
--- src/components/Ramens/index.tsx
+++ src/components/Ramens/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
-import { RouteComponentProps, withRouter } from "react-router";
-import { Redirect } from "react-router-dom";
+import { withRouter } from "react-router";
+import { Redirect, useHistory, useLocation, useParams } from "react-router-dom";
 import { parse } from "query-string";
 import { Helmet } from "react-helmet";
 import { Button, Divider, Icon } from "semantic-ui-react";
@@ -8,18 +8,17 @@ import { Button, Divider, Icon } from "semantic-ui-react";
 import { ramenData } from "../../ramenData";
 import RamenList from "./RamenList";
 
-// the left blank object is props. this time props is blank.
-// RouteComponentProps is to receive the key value handled by withRouter
-type RamensProps = {} & RouteComponentProps<{ code: string }>;
+const Ramens: FC = () => {
+  const history = useHistory();
+  const location = useLocation();
+  const { code } = useParams();
 
-const Ramens: FC<RamensProps> = ({ history, location, match }) => {
   const codes = Object.keys(ramenData);
-  const targetCode = match.params.code;
   // parse query paramte
   // when '?loading=true' is appied, isLoading is true
   const isLoading = parse(location.search).loading === "true";
 
-  return codes.includes(targetCode) ? (
+  return codes.includes(code) ? (
     <>
       <Helmet>
         <title>ramen list</title>
@@ -31,8 +30,8 @@ const Ramens: FC<RamensProps> = ({ history, location, match }) => {
         <p>Loading...</p>
       ) : (
         <RamenList
-          area={ramenData[targetCode].region}
-          ramens={ramenData[targetCode].ramens}
+          area={ramenData[code].region}
+          ramens={ramenData[code].ramens}
         />
       )}
       <Divider hidden />
